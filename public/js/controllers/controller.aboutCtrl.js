03af4581f6f2dcee3a30f23538e42b40e6f58f81
shashwat2519 <shashwat.gulyani@gmail.com>
2017-01-17 18:53:39 +0530

    finally cleaning up

--- public/js/controllers/controller.aboutCtrl.js
+++ public/js/controllers/controller.aboutCtrl.js
@@ -4,10 +4,12 @@ app.controller('aboutCtrl', function($scope){
   $(document).ready(function() {
 
     $("#cogniOwlCarousel").owlCarousel({
+      autoPlay: 5300, //Set AutoPlay to 3 seconds
       items : 4,
-      itemsDesktop : [1199,4],
-      itemsDesktopSmall : [979,4],
-      lazyLoad: true
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3],
+      lazyLoad: true,
+      jsonPath : "json/cogniData1.json"
     });
 
   });
